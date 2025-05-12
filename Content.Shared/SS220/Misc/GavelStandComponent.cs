fcf360aa9284a39d36a869f19d9f50cb0c74730a
Kawaii Selbst <54507994+KawaiiSelbst@users.noreply.github.com>
2025-05-12 18:54:24 +0300

    Minor cleanup and add licenses and description to component

--- Content.Shared/SS220/Misc/GavelStandComponent.cs
+++ Content.Shared/SS220/Misc/GavelStandComponent.cs
@@ -1,15 +1,19 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
 using System;
 using Robust.Shared.GameStates;
 
 namespace Content.Shared.SS220.Misc;
 
+/// <summary>
+/// Component that apply exclamation to people around when recieve hit from weapon with tag JudgeGavel
+/// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class GavelStandComponent : Component
 {
-    [ViewVariables(VVAccess.ReadWrite)]
     [DataField("effect")]
     public string Effect = "Exclamation";
 
     [DataField]
-    public float Distance = 0;
+    public float Distance = 5;
 }
